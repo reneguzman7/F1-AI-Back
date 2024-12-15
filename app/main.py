9c0ba7990473bb46b9967bdd32e53f2cf957daea
reneguzman7 <reneguzman7@outlook.com>
2024-12-15 13:15:24 -0500

    update CORS origin in main.py; improve comments and refactor JSON handling in predict.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # URL de tu app React
+    allow_origins=["http://localhost:5173"],  # URL de tu app React
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- app/routers/predict.py
+++ app/routers/predict.py
@@ -1,9 +1,8 @@
-import requests
-import json
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, UploadFile, File
 from pydantic import ValidationError
 from app.schemas.input_data import InputData
 from app.models.ann_model import predict_podium
+import json
 
 router = APIRouter()
 
@@ -11,12 +10,12 @@ router = APIRouter()
 @router.post("/predict")
 def get_prediction(input_data: InputData):
     try:
-        # Intentamos hacer la predicci�n con los datos
+        # Intentamos hacer la predicción con los datos
         prediction = predict_podium(input_data)
         return {"podio": prediction}
     
     except ValidationError as e:
-        # En caso de error de validaci�n, capturamos los campos incorrectos
+        # En caso de error de validación, capturamos los campos incorrectos
         errors = []
         for error in e.errors():
             field_name = error.get("loc")[0]  # El nombre del campo con el error
@@ -26,30 +25,27 @@ def get_prediction(input_data: InputData):
         # Devolvemos los errores de manera personalizada
         raise HTTPException(status_code=422, detail=errors)
 
-# Ruta para procesar el archivo JSON y devolver resultados
+# Ruta para procesar el archivo JSON enviado desde el frontend y devolver resultados
 @router.get("/send-data")
-def send_data():
-    # Ruta al archivo JSON
-    file_path = 'app\routers\data.json'
-
+async def send_data(file: UploadFile = File(...)):
     # Inicializar resultados y contador de podios
     resultados = []
     podios_contador = 0
 
     try:
-        # Leer el archivo JSON
-        with open(file_path, 'r') as file:
-            data = json.load(file)
+        # Leer el archivo JSON enviado desde el frontend
+        contents = await file.read()
+        data = json.loads(contents)
 
         # Procesar cada piloto
         for piloto in data:
             try:
-                # Si ya hay 3 podios, el resto ser� autom�ticamente "Fuera de podio"
+                # Si ya hay 3 podios, el resto será automáticamente "Fuera de podio"
                 if podios_contador >= 3:
                     resultados.append({"Piloto": piloto["Piloto"], "Resultado": "Fuera de podio"})
                     continue
 
-                # Hacer la predicci�n directamente
+                # Hacer la predicción directamente
                 input_data = InputData(**piloto)
                 prediction = predict_podium(input_data)
 
